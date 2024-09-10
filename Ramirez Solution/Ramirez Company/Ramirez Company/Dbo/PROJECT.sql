cea798168a2659e91b1f8cb349b57cad18ba26fe
DJ RAMIREZ <125239418+RamirezJunhil@users.noreply.github.com>
2024-09-10 22:30:38 +0800

    Add files via upload

---
+++ Ramirez Solution/Ramirez Company/Ramirez Company/Dbo/PROJECT.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[PROJECT]
+(
+	[Pname] VARCHAR(15) NULL , 
+    [Pnumber] INT NOT NULL, 
+    [Plocation] NCHAR(10) NOT NULL, 
+    [Dnum] INT NULL, 
+    PRIMARY KEY ([Pnumber]),
+    UNIQUE ([Pname]), 
+    CONSTRAINT [PK_PROJECT_DEPARTMENT] FOREIGN KEY ([Dnum]) REFERENCES [DEPARTMENT]([Dnumber])
+)
