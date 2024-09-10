566e16ef7d27bab4ac46880190c1c83b8adaacf8
DJ RAMIREZ <125239418+RamirezJunhil@users.noreply.github.com>
2024-09-10 23:18:10 +0800

    Add files via upload

---
+++ Ramirez Solution/Ramirez Company/Ramirez Company/Dbo/DEPENDENT.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[DEPENDENT]
+(
+	[Essn] CHAR(9) NOT NULL , 
+    [Dependent_name] VARCHAR(15) NOT NULL, 
+    [Sex] CHAR(1) NULL, 
+    [Bdate] DATE NULL, 
+    [Relationship] VARCHAR(8) NULL, 
+    PRIMARY KEY ([Dependent_name], [Essn]), 
+    CONSTRAINT [PK_DEPENDENT_EMPLOYEE] FOREIGN KEY ([Essn]) REFERENCES [EMPLOYEE]([Ssn])
+)
