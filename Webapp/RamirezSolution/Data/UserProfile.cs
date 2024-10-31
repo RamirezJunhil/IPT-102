8da40b830e37200680c6ef80ddc9995f6a850f6f
DJ RAMIREZ <Ramirez.juheu22@southernchristiancollege.edu.ph>
2024-10-31 12:34:50 +0800

    10/31/24 12:34AM

---
+++ Webapp/RamirezSolution/Data/UserProfile.cs
@@ -0,0 +1,12 @@
+﻿namespace UserProfileLibrary
+{
+    public class UserProfile
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string School { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
--- Webapp/RamirezSolution/obj/Debug/net6.0/RamirezSolution.AssemblyInfo.cs
+++ Webapp/RamirezSolution/obj/Debug/net6.0/RamirezSolution.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RamirezSolution")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a158f09fda8fad5491072b3bf8105332ad726e8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0289eef9ec4fbfe23581e2a9a732176439eba027")]
 [assembly: System.Reflection.AssemblyProductAttribute("RamirezSolution")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RamirezSolution")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
