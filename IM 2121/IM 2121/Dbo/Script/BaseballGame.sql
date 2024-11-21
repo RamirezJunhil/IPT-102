021d19ef5ac069d9f5a1480d0fea81fb123a3a3c
Ignacio-bryanlester <ignacio.brheu22@southernchristiancollege.edu.ph>
2024-11-22 06:13:04 +0800

    kkk

--- IM 2121/IM 2121/Dbo/Script/BaseballGame.sql
+++ IM 2121/IM 2121/Dbo/Script/BaseballGame.sql
@@ -12,7 +12,7 @@ USE Baseball;
 GO
 
 -- Drop Table if it Exists
-IF OBJECT_ID(N'dbo.BaseballGame', N'U') IS NOT NULL
+IF OBJECT_ID('dbo.BaseballGame') IS NOT NULL
 BEGIN
     DROP TABLE dbo.BaseballGame;
 END
@@ -21,7 +21,7 @@ GO
 -- Create Table with Constraints
 CREATE TABLE dbo.BaseballGame
 (
-    GameID NVARCHAR(10) NOT NULL,  -- Primary Key Constraint
+    GameID INT NOT NULL,  -- Primary Key Constraint
     TeamName NVARCHAR(15) NOT NULL,  -- Not Null Constraint
     GameDate DATE NOT NULL,  -- Not Null Constraint
 
@@ -30,7 +30,7 @@ CREATE TABLE dbo.BaseballGame
 GO
 
 -- Create a unique non-clustered index on the TeamName field
-CREATE UNIQUE NONCLUSTERED INDEX UX_TeamName ON dbo.BaseballGame (TeamName ASC)
+CREATE UNIQUE NONCLUSTERED INDEX UX_TeamName ON dbo.BaseballGame ([TeamName] ASC)
 WHERE TeamName IS NOT NULL;
 GO
 
@@ -43,9 +43,9 @@ GO
 BEGIN
     INSERT INTO dbo.BaseballGame (GameID, TeamName, GameDate)
     VALUES
-        ('1', 'CCIS', '2024-11-21'),
-        ('2', 'CBA', '2024-11-21'),
-        ('3', 'CTE', '2024-11-21');
+        ('1', 'SCC', '2024-10-21'),
+        ('2', 'NDMC', '2024-10-21'),
+        ('3', 'USM', '2024-10-21');
 END
 GO
 
@@ -55,14 +55,11 @@ GO
 
 -- Update the GameDate for a specific record
 UPDATE dbo.BaseballGame
-SET GameDate = '2024-11-22'
+SET GameDate = '2024-11-25'
 WHERE GameID = '1';  -- Only update GameID '1'
 GO
-
--- Check the updated row
 SELECT * FROM dbo.BaseballGame
-WHERE GameID = '1';
-GO
+
 
 -- Delete the record from the BaseballGame table where GameID is '3'
 DELETE FROM dbo.BaseballGame
